8d0f8f2a500c4553b2f6a40e1bbbdd0e11cdff40
Adedero <cosmosjunior0495@gmail.com>
2026-01-16 20:31:14 +0100

    install djs-selfbot-v13

--- src/index.js
+++ src/index.js
@@ -1,36 +1,28 @@
-import { config } from "dotenv";
-import { Client } from "selfbot-discord";
+import { Client, Channel } from "djs-selfbot-v13";
 import logger from "./lib/logger.js";
-import pkg from "selfbot-discord";
+import { Env } from "./lib/env.js";
+import { config } from "dotenv";
+
+config({ quiet: true });
 
 function main() {
-  config({ quiet: true });
-
-  const client = new Client();
+  const TOKEN = Env.get("DISCORD_TOKEN");
+  const CHANNEL_ID = Env.get("ALERT_CHANNEL_ID");
   /**
-   * @type {pkg.Channel|null}
+   * @type {Channel|null}
    */
   let channel = null;
-  const TOKEN = process.env.DISCORD_TOKEN;
-  const CHANNEL_ID = process.env.ALERT_CHANNEL_ID;
-  if (!TOKEN) {
-    logger.error("DISCORD_TOKEN environment variable is not set");
-    return;
-  }
 
-  if (!CHANNEL_ID) {
-    logger.error("ALERT_CHANNEL_ID environment variable is not set");
-    return;
-  }
+  const client = new Client();
 
   client.on("ready", async () => {
-    channel = client.channels.find((c) => c.id === CHANNEL_ID);
+    logger.info("Bot is online");
+    channel = client.channels.cache.get(CHANNEL_ID);
     if (channel) {
-      await channel.send("Bot is online!");
+      await channel.send("Bot is online");
     }
-    logger.info(`Bot is online as ${client.user.tag}`);
   });
-
+  
   client.on("guildMemberAdd", async (member) => {
     try {
       if (!channel) {
@@ -42,7 +34,7 @@ function main() {
     } catch (error) {
       logger.error(`Error sending join alert: ${error}`);
     }
-  });
+  })
 
   client.login(TOKEN);
 }
---
+++ src/lib/env.js
@@ -0,0 +1,19 @@
+export const Env = {
+  /**
+   *
+   * @param {string} value
+   * @param {string} defaultValue
+   * @returns {string}
+   */
+  get(value, defaultValue) {
+    const envValue = process.env[value];
+    if (!envValue) {
+      if (defaultValue) {
+        return defaultValue;
+      } else {
+        throw new Error(`Environment variable ${value} not found`);
+      }
+    }
+    return envValue;
+  },
+};
